e90dfa39d2ad425566987241d60ed93b12ceaacb
jasminpatel <jasmin.techie@gmail.con>
2020-04-18 21:40:30 +0530

    Complete Project

---
+++ ios/Runner/Runner-Bridging-Header.h
@@ -0,0 +1,5 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <assets_audio_player/assets_audio_player-Swift.h>
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playAudio({int soundNumber}) {
+    final assetsAudioPlayer = AssetsAudioPlayer();
+    assetsAudioPlayer.open(
+      Audio("assets/note$soundNumber.wav"),
+    );
+    assetsAudioPlayer.play();
+  }
+
+  Widget buildKey({int soundNumber, Color color}) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          playAudio(soundNumber: soundNumber);
+        },
+        color: color,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(soundNumber: 1, color: Colors.green),
+              buildKey(soundNumber: 2, color: Colors.yellow),
+              buildKey(soundNumber: 3, color: Colors.red),
+              buildKey(soundNumber: 4, color: Colors.teal),
+              buildKey(soundNumber: 5, color: Colors.blue),
+              buildKey(soundNumber: 6, color: Colors.orange),
+              buildKey(soundNumber: 7, color: Colors.purple),
+            ],
+          ),
         ),
       ),
     );
